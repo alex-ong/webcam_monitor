a5d8fb201c28b0fae44f696beb2150d2b1c06560
alexo <alex-ong>
2021-11-29 12:41:26 +1100

    fmt: Fix formatting and comment the code more

--- status.py
+++ status.py
@@ -1,7 +1,13 @@
-from winreg import *
+"""
+Simple daemon that prints out
+the current appplication that is using the webcam
+"""
+import winreg as wr
 import time
+
 LAST_USED_TIME_STOP = "LastUsedTimeStop"
 ROOT_KEY = r"SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\webcam"
+
 
 def iterate_leaf(key):
     """
@@ -9,24 +15,25 @@ def iterate_leaf(key):
     Returns true it ocntains LastUsedTimeStop and its value is 0x00000000,
     As this indicates that a camera is in use.
     """
-    _, num_subvalues, __ = QueryInfoKey(key)
+    _, num_subvalues, __ = wr.QueryInfoKey(key)
     for i in range(num_subvalues):
-        name, value, _ = EnumValue(key, i)        
-        if name == LAST_USED_TIME_STOP and value == 0:        
+        name, value, _ = wr.EnumValue(key, i)
+        if name == LAST_USED_TIME_STOP and value == 0:
             return True
-            
+
     return False
+
 
 def iterate_middle_node(key):
     """
     Recursively iterates a non-leaf node.
     Returns name of application currently accessing the webcam.
     """
-    num_subkeys, _, __ = QueryInfoKey(key)
-    
-    for i in range(num_subkeys):        
-        subkey_name = EnumKey(key, i)
-        with OpenKey(key, subkey_name) as subkey:            
+    num_subkeys, _, __ = wr.QueryInfoKey(key)
+
+    for i in range(num_subkeys):
+        subkey_name = wr.EnumKey(key, i)
+        with wr.OpenKey(key, subkey_name) as subkey:
             if subkey_name == "NonPackaged":
                 sub_result = iterate_middle_node(subkey)
                 if sub_result is not None:
@@ -35,21 +42,22 @@ def iterate_middle_node(key):
                 sub_result = iterate_leaf(subkey)
                 if sub_result:
                     return subkey_name
-    
+
     return None
-            
+
+
 def iterate_main():
     """
     Returns the name of the application that is using the webcam,
     or None if it is not in use.
     """
-    aReg = ConnectRegistry(None, HKEY_CURRENT_USER)
-    with OpenKey(aReg, ROOT_KEY) as aKey:
-        result = iterate_middle_node(aKey)
+    registry= wr.ConnectRegistry(None, wr.HKEY_CURRENT_USER)
+    with wr.OpenKey(registry, ROOT_KEY) as key:
+        result = iterate_middle_node(key)
     return result
-    
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
 
     while True:
         print(iterate_main())
